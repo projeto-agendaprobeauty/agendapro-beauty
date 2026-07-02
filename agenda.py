6f8f158680670651a114fd5cb410bc46681e224d
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-02 11:17:07 -0300

    corrige a definição da classe Agenda para incluir valor padrão em horario_final

--- agenda.py
+++ agenda.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field, EmailStr
 class Agenda(BaseModel):
     status: str
     horario_inicial: str
-    horario_final: str
-    data: str
-    cliente_id: int
-    profissional_id: int
-    servico_id: int
+    horario_final: str = 
+    data: str = Field(min_length=10, max_length=10)
+    cliente_id: int 
+    profissional_id: int 
+    servico_id: int 
     
