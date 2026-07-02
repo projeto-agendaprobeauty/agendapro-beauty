54f69473efa3c56b976ffa60817ccad4c7a73afd
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-02 11:19:35 -0300

    remove tabela e id da classe AreaProfissao

--- area_profissao.py
+++ area_profissao.py
@@ -2,10 +2,9 @@ from sqlalchemy import Column, Integer, String
 from banco_dados import Base
 
 class AreaProfissao(Base):
-    __tablename__ = "area_profissao"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nome_area = Column(String(100), nullable=False)
+    
+   
+    nome_area: std  = Column(String(100), nullable=False)
     
      
 
