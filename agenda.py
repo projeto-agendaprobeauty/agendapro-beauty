8ebd8f9bbc2efba58c1289a22866facbdc3d877a
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-02 11:17:41 -0300

    remove valor padrão da propriedade horario_final na classe Agenda

--- agenda.py
+++ agenda.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, EmailStr
 class Agenda(BaseModel):
     status: str
     horario_inicial: str
-    horario_final: str = 
+    horario_final: str 
     data: str = Field(min_length=10, max_length=10)
     cliente_id: int 
     profissional_id: int 
