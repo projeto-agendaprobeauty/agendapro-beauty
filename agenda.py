abad3f969181a54eca94c62e838fb0e94799cdf7
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-02 11:27:48 -0300

    altera tipo da propriedade data na classe Agenda de int para date

--- agenda.py
+++ agenda.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, Field, EmailStr
+from datetime import date
+
 
 
 class Agenda(BaseModel):
     status: str
     horario_inicial: str
     horario_final: str 
-    data: int
+    data: date
     cliente_id: int 
     profissional_id: int 
     servico_id: int 
