83db4b5d63f14577758745538a4f725a6b96475b
Emilli <emillisant00oo@gmail.com>
2026-07-06 20:06:32 -0300

    padronizei os nomes

--- classes/clientes.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field, EmailStr
-
-# Schema de Cliente
-class Cliente(BaseModel):
-    nome_cliente: str = Field(min_length=2)
-    email: str
-    cidade: str
--- classes/profissionais.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, EmailStr
-
-
-class Profissional(BaseModel):
-    nome_profissional: str = Field(min_length=2)
-    email: str
-    horario_inicial: str 
-    horario_final: str 
