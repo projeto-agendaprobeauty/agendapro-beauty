3d1f852574a85656d419c3b8f390106b973a6a12
Emilli <emillisant00oo@gmail.com>
2026-07-07 10:24:11 -0300

    criei rotas insert, select all e select por id

---
+++ routers/profissional.py
@@ -0,0 +1,63 @@
+import os
+from fastapi import APIRouter
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+router = APIRouter(prefix='/profissional', tags=['Profissional'])
+from classes.profissional import Profissional
+
+#CREATE
+@router.post('')
+def insert_profissional(profissional :Profissional):
+  engine = create_engine(DATABASE_URL)
+  try:
+    with engine.begin() as con:
+      sql = """INSERT INTO public.profissional
+                (nome, email, horario_inicio, horario_fim)
+                VALUES(':nome', ':email', ':horario_inicio', ':horario_fim');"""
+      dados = {
+        "nome_profissional": profissional.nome,
+        "email":profissional.email,
+        "horario_inicial": profissional.horario_inicio,
+        "horario_final": profissional.horario_fim
+      }
+      con.execute(text(sql),dados)
+  except Exception as erro:
+    return erro
+  engine.dispose()
+  return 'Profissional cadastrado com sucesso!'
+
+#READ
+@router.get('')
+def select_profissional():
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.connect() as con:
+            sql = """SELECT id, nome, email, horario_inicio, horario_fim
+                      FROM public.profissional;"""
+            response = con.execute(text(sql))
+            result = response.mappings().all()
+    except Exception as erro:
+        return erro
+    engine.dispose()
+    return result
+  
+#READ (buscar profissonal por id)
+@router.get('/{id}')
+def search_cliente(id : int):
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.connect() as con:
+            sql = """SELECT nome, email, horario_inicio, horario_fim 
+                    FROM public.profissional 
+                    WHERE id = :id"""
+            response = con.execute(text(sql), {"id": id})
+            result = response.fetchone()
+    except Exception as erro:
+        return erro
+    engine.dispose()
+    return result._mapping
+
+#UPDATE
