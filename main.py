9f6b42d0a7636806b73054d7bef95fa6f162958f
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-07 11:50:54 -0300

    corrige configuração do servidor para usar host e porta corretos em main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def index():
     return {"Hello" : "World"}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app",
-                #host='0.0.0.0',
-                port=80,
-                reload=True)
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True)
