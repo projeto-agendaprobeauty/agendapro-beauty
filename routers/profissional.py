5ee7b66e873cf0e4ee3ff568f657a023c7cdd860
Emilli <emillisant00oo@gmail.com>
2026-07-07 11:43:40 -0300

    finalizei CRUD profissional e arrumei erros de escrita/sintaxe

--- routers/profissional.py
+++ routers/profissional.py
@@ -11,23 +11,23 @@ from classes.profissional import Profissional
 #CREATE
 @router.post('')
 def insert_profissional(profissional :Profissional):
-  engine = create_engine(DATABASE_URL)
-  try:
-    with engine.begin() as con:
-      sql = """INSERT INTO public.profissional
-                (nome, email, horario_inicio, horario_fim)
-                VALUES(':nome', ':email', ':horario_inicio', ':horario_fim');"""
-      dados = {
-        "nome_profissional": profissional.nome,
-        "email":profissional.email,
-        "horario_inicial": profissional.horario_inicio,
-        "horario_final": profissional.horario_fim
-      }
-      con.execute(text(sql),dados)
-  except Exception as erro:
-    return erro
-  engine.dispose()
-  return 'Profissional cadastrado com sucesso!'
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.begin() as con:
+            sql = """INSERT INTO public.profissional
+                    (nome, email, horario_inicio, horario_fim)
+                    VALUES(:nome, :email, :horario_inicio, :horario_fim);"""
+            dados = {
+            "nome": profissional.nome,
+            "email":profissional.email,
+            "horario_inicio": profissional.horario_inicio,
+            "horario_fim": profissional.horario_fim
+            }
+            con.execute(text(sql),dados)
+    except Exception as erro:
+        return erro
+    engine.dispose()
+    return 'Profissional cadastrado com sucesso!'
 
 #READ
 @router.get('')
@@ -36,17 +36,17 @@ def select_profissional():
     try:
         with engine.connect() as con:
             sql = """SELECT id, nome, email, horario_inicio, horario_fim
-                      FROM public.profissional;"""
+                    FROM public.profissional;"""
             response = con.execute(text(sql))
             result = response.mappings().all()
     except Exception as erro:
         return erro
     engine.dispose()
     return result
-  
+
 #READ (buscar profissonal por id)
 @router.get('/{id}')
-def search_cliente(id : int):
+def search_profissional(id : int):
     engine = create_engine(DATABASE_URL)
     try:
         with engine.connect() as con:
@@ -61,3 +61,42 @@ def search_cliente(id : int):
     return result._mapping
 
 #UPDATE
+@router.put('/{id}')
+def update_profissional(id: int, profissional :Profissional):
+    print ("entreii")
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.begin() as con: 
+            sql = """UPDATE public.profissional
+                    SET nome = :nome, 
+                        email = :email, 
+                        horario_inicio = :horario_inicio,
+                        horario_fim = :horario_fim
+                    WHERE id = :id"""     
+            print(profissional)       
+            dados = {
+                "id": id,
+                "nome": profissional.nome,
+                "email":profissional.email,
+                "horario_inicio": profissional.horario_inicio,
+                "horario_fim": profissional.horario_fim
+            }
+            con.execute(text(sql), dados)
+    except Exception as erro:
+        return "erro:"+erro
+    engine.dispose()
+    return 'Profissional atualizado com sucesso!'
+
+#DELETE
+@router.delete('/{id}')
+def delete_profissional(id : int):
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.begin() as con:
+            sql = """DELETE FROM public.profissional
+                    WHERE id=:id;"""
+            con.execute(text(sql), {"id": id})
+            return 'Profissional deletado com sucesso!'
+    except Exception as erro:
+        return erro
+    
