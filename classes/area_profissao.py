9edf0636eb80a14167b29b9a5e77faf4144524f1
Emilli <emillisant00oo@gmail.com>
2026-07-09 11:48:00 -0300

    CRUD servico

--- classes/area_profissao.py
+++ classes/area_profissao.py
@@ -2,9 +2,5 @@ from sqlalchemy import Column, Integer, String
 from banco_dados import Base
 
 class AreaProfissao(Base):
-    
-   
     nome_area: str  = Column(String(100), nullable=False)
-    
-     
 
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from routers.cliente import router as cliente_router
 from routers.agenda import router as agenda_router
 from routers.profissional import router as profissional_router
 from routers.area import router as area_router
+from routers.servico import router as servico_router
 
 app = FastAPI()
 
 app.include_router(cliente_router)
 app.include_router(agenda_router)
-
-
+app.include_router(servico_router)
 app.include_router(profissional_router)
 app.include_router(area_router)
+
 @app.get('/')
 def index():
     return {"Hello" : "World"}
@@ -21,5 +22,5 @@ if __name__ == '__main__':
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
-        port=8000,
+        port=80,
         reload=True)
