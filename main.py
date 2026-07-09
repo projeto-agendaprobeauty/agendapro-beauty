9f0e43b04d353a1a16c097a824db95b0eed9d239
Koucher <166264221+DaviKM@users.noreply.github.com>
2026-07-09 10:45:13 -0300

    Implement CRUD for scheduling and fix property naming issues (#13)
    * corrige tipo da propriedade nome_area para str na classe AreaProfissao
    
    * implementa CRUD para agendamentos na rota /agenda
    
    * adiciona nova linha em agenda.py e remove linha em branco no final do arquivo
    
    * corrige nome da propriedade horario_inicio para horario_inicial nas funções de agendamento
    
    * adiciona importação do router da agenda em main.py
    
    * adiciona nova linha em main.py para melhorar a legibilidade
    
    * adiciona prints para exibir diretório atual e DATABASE_URL em agenda.py
    
    * corrige configuração do servidor para usar host e porta corretos em main.py
    
    * adiciona impressão do número de linhas afetadas ao cadastrar agendamento
    
    * adiciona impressão do número de linhas afetadas ao cadastrar agendamento
    
    * corrige nome do campo 'horario_fim' para 'horario_final' ao cadastrar agendamento
    
    * corrige atribuição do campo 'horario_fim' para 'horario_final' ao cadastrar agendamento
    
    * corrige nome do campo 'horario_fim' para 'horario_final' ao cadastrar e listar agendamentos
    
    * corrige nome do campo 'horario_fim' para 'horario_final' nas funções de busca e atualização de agendamentos
    
    ---------
    
    Co-authored-by: Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 from routers.cliente import router as cliente_router
+from routers.agenda import router as agenda_router
 from routers.profissional import router as profissional_router
 from routers.area import router as area_router
 
 app = FastAPI()
+
 app.include_router(cliente_router)
+app.include_router(agenda_router)
+
+
 app.include_router(profissional_router)
 app.include_router(area_router)
 @app.get('/')
@@ -13,7 +18,8 @@ def index():
     return {"Hello" : "World"}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app",
-                #host='0.0.0.0',
-                port=80,
-                reload=True)
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True)
