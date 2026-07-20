ceb8a6910f19019c6ee2f8e0487702c71001ceee
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-20 15:44:06 -0300

    remove função de agendamento de serviço e redefine modelo Agenda

--- classes/agenda.py
+++ classes/agenda.py
@@ -1,26 +1,11 @@
-f@router.post('/agendar')
-def agendar_servico(agenda :Agenda):
-    try:
-        with engine.begin() as con:
-            sql = """INSERT INTO public.agenda
-                    (status, horario_inicial, horario_final, data, cliente_id, profissional_id, servico_id)
-                VALUES ( :status, :horario_inicial, :horario_final, :data, :cliente_id, :profissional_id, :servico_id)"""            
-            dados = {
-                "status": agenda.status,
-                "horario_inicial": agenda.horario_inicial,
-                "horario_final": agenda.horario_final,
-                "data": agenda.data,
-                "cliente_id": agenda.cliente_id,
-                "profissional_id": agenda.profissional_id,
-                "servico_id": agenda.servico_id
-            }
+from datetime import date
+from pydantic import BaseModel
 
-            resultado = con.execute(text(sql), dados)
-            print("Linhas afetadas:", resultado.rowcount)
-
-    except Exception as erro:
-        print("ERRO:", erro)
-        return {"erro": str(erro)}
-        
-    engine.dispose()
-    return 'Serviço agendado com sucesso!'
+class Agenda(BaseModel):
+    status: str
+    horario_inicial: str
+    horario_final: str
+    data: date
+    cliente_id: int
+    profissional_id: int
+    servico_id: int
