6172d04e6b2ed06434c72155542905f62e2ec21c
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-07-20 15:33:49 -0300

    adiciona função para agendar serviço com persistência no banco de dados

--- classes/agenda.py
+++ classes/agenda.py
@@ -1,17 +1,26 @@
-from pydantic import BaseModel, field_validator
-from datetime import date, time
+f@router.post('/agendar')
+def agendar_servico(agenda :Agenda):
+    try:
+        with engine.begin() as con:
+            sql = """INSERT INTO public.agenda
+                    (status, horario_inicial, horario_final, data, cliente_id, profissional_id, servico_id)
+                VALUES ( :status, :horario_inicial, :horario_final, :data, :cliente_id, :profissional_id, :servico_id)"""            
+            dados = {
+                "status": agenda.status,
+                "horario_inicial": agenda.horario_inicial,
+                "horario_final": agenda.horario_final,
+                "data": agenda.data,
+                "cliente_id": agenda.cliente_id,
+                "profissional_id": agenda.profissional_id,
+                "servico_id": agenda.servico_id
+            }
 
-class Agenda(BaseModel):
-    status: str
-    horario_inicial: time
-    horario_final: time
-    data: date
-    cliente_id: int 
-    profissional_id: int 
-    servico_id: int 
+            resultado = con.execute(text(sql), dados)
+            print("Linhas afetadas:", resultado.rowcount)
 
-    @field_validator('status')
-    def validaStatus(cls, value : str) -> str:
-        if value == 'Marcado' or value == 'Cancelado' or value == 'Realizado':
-            return value
-        raise ValueError('Valor de status inválido')
+    except Exception as erro:
+        print("ERRO:", erro)
+        return {"erro": str(erro)}
+        
+    engine.dispose()
+    return 'Serviço agendado com sucesso!'
