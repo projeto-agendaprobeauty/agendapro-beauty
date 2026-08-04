bb0ff6cd9298586eae7a83594482bf401cf6c2bb
Emilli <emillisant00oo@gmail.com>
2026-08-04 10:36:15 -0300

    Read, Update e delete USUARIO feitos

--- routers/usuario.py
+++ routers/usuario.py
@@ -28,3 +28,72 @@ def insert_usuario(usuario :Usuario):
         return erro
     engine.dispose()
     return 'Usuário cadastrado com sucesso!'
+  
+#READ
+@router.get('')
+def select_usuario():
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.connect() as con:
+            sql = """SELECT id, nome, senha, email, telefone
+                      FROM public.usuario;"""
+            response = con.execute(text(sql))
+            result = response.mappings().all()
+    except Exception as e:
+        return e
+    engine.dispose()
+    return result
+  
+#READ (busca usuario por id)
+@router.get('/{id}')
+def search_usuario(id : int):
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.connect() as con:
+            sql = """SELECT nome, email, telefone 
+                    FROM public.usuario 
+                    WHERE id = :id;"""
+            response = con.execute(text(sql), {"id": id})
+            result = response.fetchone()
+    except Exception as erro:
+        return erro
+    engine.dispose()
+    return result._mapping
+  
+#UPDATE
+@router.put('/{id}')
+def update_usuario(id: int, usuario :Usuario):
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.begin() as con: 
+            sql = """UPDATE public.usuario
+                    SET nome= :nome,
+                    senha= :senha, 
+                    email= :email,
+                    telefone = :telefone,
+                    WHERE id = :id;"""            
+            dados = {
+                "id": id, 
+                "nome": usuario.nome,
+                "senha": usuario.senha,
+                "email": usuario.email,
+                "telefone": usuario.telefone
+            }
+            con.execute(text(sql), dados)
+    except Exception as erro:
+        return erro
+    engine.dispose()
+    return 'Usuário atualizado com sucesso!'
+  
+#DELETE
+@router.delete('/{id}')
+def delete_usuario(id : int):
+    engine = create_engine(DATABASE_URL)
+    try:
+        with engine.begin() as con:
+            sql = """DELETE FROM usuario
+                    WHERE id=:id;"""
+            con.execute(text(sql), {"id": id})
+            return 'Usuário deletado com sucesso!'
+    except Exception as erro:
+        return erro
