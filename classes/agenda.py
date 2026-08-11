f4e154baefb033bf693538d271e6f8f69c83b54e
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-11 09:49:56 -0300

    remove código desnecessário e ajusta a estrutura da classe Agenda

--- classes/agenda.py
+++ classes/agenda.py
@@ -1,5 +1,4 @@
 from datetime import date, time
-<<<<<<< HEAD
 from typing import Literal
 from pydantic import BaseModel, Field, field_validator, model_validator
 
@@ -47,24 +46,3 @@ class Agenda(BaseModel):
         
         return self
    
-=======
-from pydantic import BaseModel, field_validator
-
-class Agenda(BaseModel):
-    status: str
-    horario_inicial: time
-    data: date
-    cliente_id: int 
-    profissional_id: int 
-    servico_id: int 
-
-    @field_validator('status')
-    def validaStatus(cls, value : str) -> str:
-        if value == 'Marcado' or value == 'Cancelado' or value == 'Realizado':
-            return value
-        raise ValueError('Valor de status inválido')
-
-    @field_validator('horario_inicial')
-    def formataHorario(cls, value : time) -> time:
-        return value.strftime("%H:%M")
->>>>>>> origin/develop
