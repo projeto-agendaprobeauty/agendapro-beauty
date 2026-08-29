01a295f8b6cf1e3dac7c2a8350af51f56f8013d6
Everton Luiz Bianchi Siqueira <evertonbianchi5@gmail.com>
2026-08-29 18:22:16 -0300

    refatora validação de telefone e formatação de campos na classe Usuario

--- classes/usuario.py
+++ classes/usuario.py
@@ -1,45 +1,54 @@
-from pydantic import BaseModel, Field, EmailStr, field_validator, model_validator
 from typing import Literal
+from pydantic import BaseModel, Field, EmailStr, field_validator
+
 
 class Usuario(BaseModel):
-  nome: str = Field(min_length=3)
-  senha: str = Field(min_length=6, max_length= 16)
-  email: EmailStr
-  telefone: str = Field(min_length=12, max_length= 12)
-  tipo_usuario: Literal['cliente', 'profissional', 'admin']
 
-  
-  @field_validator('nome')
-  def nomeEspaco(cls, value: str) -> str:
-    if value.count(' ') == 0:
-      raise ValueError('O nome deve conter pelo menos um sobrenome')
-    return value.title()
-  
-@field_validator('telefone')
-def validaTelefone(cls, value: str) -> str:
+    nome: str = Field(min_length=3)
 
-    partes = value.split()
+    senha: str = Field(min_length=6, max_length=16)
 
-    if len(partes) != 2:
-        raise ValueError(
-            'Digite o telefone no formato: 51 991155248'
-        )
+    email: EmailStr
 
-    ddd, numero = partes
+    telefone: str
 
-    if not ddd.isdigit() or not numero.isdigit():
-        raise ValueError(
-            'DDD e telefone devem conter apenas números'
-        )
+    tipo_usuario: Literal['cliente', 'profissional', 'admin']
 
-    if len(ddd) != 2:
-        raise ValueError(
-            'O DDD deve possuir 2 números'
-        )
 
-    if len(numero) != 9:
-        raise ValueError(
-            'O telefone deve possuir 9 números'
-        )
+    @field_validator('nome')
+    def nomeEspaco(cls, value: str) -> str:
 
-    return value
+        if value.count(' ') == 0:
+            raise ValueError('O nome deve conter pelo menos um sobrenome')
+
+        return value.title()
+
+
+    @field_validator('telefone')
+    def validaTelefone(cls, value: str) -> str:
+
+        partes = value.split()
+
+        if len(partes) != 2:
+            raise ValueError(
+                'Digite o telefone no formato: 51 991155248'
+            )
+
+        ddd, numero = partes
+
+        if not ddd.isdigit() or not numero.isdigit():
+            raise ValueError(
+                'DDD e telefone devem conter apenas números'
+            )
+
+        if len(ddd) != 2:
+            raise ValueError(
+                'O DDD deve possuir 2 números'
+            )
+
+        if len(numero) != 9:
+            raise ValueError(
+                'O telefone deve possuir 9 números'
+            )
+
+        return value
